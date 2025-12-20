53d5e404b9d83061354d0fa812e5fba83aa4e54d
bastien-winant <armand.winant@gmail.com>
2025-12-20 23:52:31 +0100

    use airflow variables for credentials retrieval

--- dags/dlt_pipeline.py
+++ dags/dlt_pipeline.py
@@ -1,11 +1,12 @@
 import dlt
+from airflow.models import Variable
 from dlt.sources.rest_api import RESTAPIConfig, rest_api_source
 
 config: RESTAPIConfig = {
 	"client": {
 		"base_url": "https://api.github.com",
 		"auth": {
-			"token": dlt.secrets["sources.access_token"],
+			"token": Variable.get("github_access_token"),
 		},
 		"headers": {
 			"Accept": "application/vnd.github+json",
